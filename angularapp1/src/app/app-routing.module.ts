bae999810233d1c37400e5199f95b07fd4b67fe4
mahajan-programming <shreyas.official07@gmail.com>
2022-02-15 16:13:50 +0530

    viewacademy student page added

--- angularapp1/src/app/app-routing.module.ts
+++ angularapp1/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AdminacademyComponent } from './adminacademy/adminacademy.component';
-
+import { ViewacademyComponent } from './viewacademy/viewacademy.component';
 const routes: Routes = [
   {path:'',redirectTo:'homepage',pathMatch:'full'},
   {path:'adminacademy',component:AdminacademyComponent},
   {path:'homepage',component:HomepageComponent},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
+  {path:'viewacademy', component:ViewacademyComponent}
 ];
 
 @NgModule({
