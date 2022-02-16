994f95cdfa57e3a6e6bee0c61ee6e35b37aecf8f
subash1761 <18tuit145@skct.edu.in>
2022-02-16 20:31:26 +0530

    Institute,AddAcademy,UpdateAcademy added

---
+++ angularapp1/src/app/addacademy/addacademy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addacademy',
+  templateUrl: './addacademy.component.html',
+  styleUrls: ['./addacademy.component.css']
+})
+export class AddacademyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angularapp1/src/app/app-routing.module.ts
+++ angularapp1/src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AdminacademyComponent } from './adminacademy/adminacademy.component';
 import { ViewacademyComponent } from './viewacademy/viewacademy.component';
 import { DisplayuserComponent } from './displayuser/displayuser.component';
+import { InstituteComponent } from './institute/institute.component';
+import { AddacademyComponent } from './addacademy/addacademy.component';
+import { UpdateacademyComponent } from './updateacademy/updateacademy.component';
 const routes: Routes = [
   {path:'',redirectTo:'homepage',pathMatch:'full'},
   {path:'adminacademy',component:AdminacademyComponent},
@@ -13,7 +16,10 @@ const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
   {path:'viewacademy', component:ViewacademyComponent},
-  {path:'displayuser',component:DisplayuserComponent}
+  {path:'displayuser',component:DisplayuserComponent},
+  {path:'institute' ,component:InstituteComponent},
+  {path:'addacademy' ,component:AddacademyComponent},
+  {path:'updateacademy' ,component:UpdateacademyComponent}
 ];
 
 @NgModule({
