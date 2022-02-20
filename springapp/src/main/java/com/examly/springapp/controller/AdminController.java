06f210145bd30cdd9b9c6aa407eeabcd56ef3c8a
mahajan-programming <shreyas.official07@gmail.com>
2022-02-20 18:41:00 +0530

    admin controller file updated

--- springapp/src/main/java/com/examly/springapp/controller/AdminController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AdminController.java
@@ -1,4 +1,18 @@
 package com.examly.springapp.controller;
 
+import com.examly.springapp.model.InstituteModel;
+import com.examly.springapp.service.InstituteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class AdminController {
+
+    @Autowired
+    private InstituteService instituteService;
+
+    @GetMapping("/institutes")
+    public List<InstituteModel> getAllInstitute(){ return instituteService.getAllInstitutes(); }
 }
+
