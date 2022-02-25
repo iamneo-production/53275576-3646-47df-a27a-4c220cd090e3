01e4ff57b1a30727f4c7c38ba5bc1367a19f7d5c
Parthasarathy S <sparthasarathy2001@gmail.com>
2022-02-25 19:41:10 +0530

    addacademy,addcourse,adminacademy validation added

--- angularapp1/src/app/login/login.component.ts
+++ angularapp1/src/app/login/login.component.ts
@@ -19,7 +19,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginform = new FormGroup({
       email : new FormControl('', [Validators.required, Validators.email,
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+        Validators.pattern('^([a-zA-Z]+.*?)+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
       password : new FormControl('', [Validators.required,
         Validators.pattern('^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+*!=]).*$') ]),
     });
--- angularapp1/src/app/signup/signup.component.ts
+++ angularapp1/src/app/signup/signup.component.ts
@@ -38,7 +38,7 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
     this.signupform = new FormGroup({
       username : new FormControl('', [Validators.required,Validators.minLength(6),Validators.pattern('^[a-z0-9_-]{8,15}$')]),
-      email : new FormControl('', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+      email : new FormControl('', [Validators.required, Validators.email,Validators.pattern('^([a-zA-Z]+.*?)+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
       mobileNumber : new FormControl('',
         [
           Validators.required,
@@ -50,6 +50,7 @@ export class SignupComponent implements OnInit {
           Validators.pattern('^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+*!=]).*$')])
     });
   }
+  // ^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+*!=]).*$ [a-z0-9._%+-]
   saveUser(){
     this.userService.createUser(this.user).subscribe(data=>{
       console.log(data)
