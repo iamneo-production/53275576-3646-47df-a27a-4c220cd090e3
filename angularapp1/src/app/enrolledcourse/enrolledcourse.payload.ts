efb0a799c27211fee40be433711ffa134e12f87a
senthilkumar <senthillkumar359@gmail.com>
2022-03-02 12:43:19 +0530

    enrolledcourse updated

---
+++ angularapp1/src/app/enrolledcourse/enrolledcourse.payload.ts
@@ -0,0 +1,18 @@
+export class Enrolledcoursepayload{
+    firstName!:string;
+    lastName!:string;
+    fatherName!:string;
+    motherName!:string;
+    phoneNumber1!:string;
+    emailId!:string;
+    age!:string;
+    phoneNumber2!:string;
+    SSLCorHSCMarks!:string;
+    maleorfemale!:string;
+    houseNo!:string;
+    streetName!:string;
+    areaName!:string;
+    pincode!:string;
+    state!:string;
+    nationality!:string;
+}
