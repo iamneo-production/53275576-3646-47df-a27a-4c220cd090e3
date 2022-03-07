b1558375499b8c2c19a91df160389e6670e20bae
Bala513010 <81249169+Bala513010@users.noreply.github.com>
2022-03-07 12:10:44 +0530

    Update student.component.spec.ts

--- angularapp1/src/app/student/student.component.spec.ts
+++ angularapp1/src/app/student/student.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StudentComponent } from './Student.component';
+import { StudentComponent } from './student.component';
 
 describe('StudentComponent', () => {
   let component: StudentComponent;
