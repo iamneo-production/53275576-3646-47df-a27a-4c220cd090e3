3788735c012291df8cf61b47257ad46b55f705a6
vtu11128 <83691018+vtu11128@users.noreply.github.com>
2022-03-08 10:58:58 +0530

    Update updatestudent.component.spec.ts

--- angularapp1/src/app/updatestudent/updatestudent.component.spec.ts
+++ angularapp1/src/app/updatestudent/updatestudent.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import  { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UpdatestudentComponent } from './updatestudent.component';
+import  { UpdatestudentComponent } from './updatestudent.component';
 
-describe('UpdatestudentComponent', () => {
-  let component: UpdatestudentComponent;
-  let fixture: ComponentFixture<UpdatestudentComponent>;
+describe('UpdatestudentComponent', () =>  {
+  let component: UpdatestudentComponent ;
+  let fixture: ComponentFixture<UpdatestudentComponent> ;
 
-  beforeEach(async () => {
+  beforeEach(async () =>  {
     await TestBed.configureTestingModule({
       declarations: [ UpdatestudentComponent ]
     })
     .compileComponents();
-  });
+  }) ;
 
-  beforeEach(() => {
+  beforeEach(() =>  {
     fixture = TestBed.createComponent(UpdatestudentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }) ;
 
-  it('should create', () => {
+  it('should create', () =>  {
     expect(component).toBeTruthy();
-  });
-});
+  }) ;
+}) ;
