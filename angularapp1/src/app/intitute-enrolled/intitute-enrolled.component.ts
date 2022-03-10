ee5b23cf132e5f5e6e86faa1a5db3558c66c2521
mahajan-programming <shreyas.official07@gmail.com>
2022-03-10 14:17:20 +0530

    institute-enrolled fronted maid dynamic

--- angularapp1/src/app/intitute-enrolled/intitute-enrolled.component.ts
+++ angularapp1/src/app/intitute-enrolled/intitute-enrolled.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { Course } from '../service/course';
+import { CourseService } from '../service/course.service';
 @Component({
   selector: 'app-intitute-enrolled',
   templateUrl: './intitute-enrolled.component.html',
   styleUrls: ['./intitute-enrolled.component.css']
 })
 export class IntituteEnrolledComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  courses : Course[];
+  constructor(private router: Router, private courseService:CourseService) { }
 
   ngOnInit(): void {
+    this.getCourse();
+  }
+
+  private getCourse(){
+    this.courseService.getCourseList().subscribe(data=>{
+      this.courses=data;
+    });
   }
 
   enrolledCourse(){
