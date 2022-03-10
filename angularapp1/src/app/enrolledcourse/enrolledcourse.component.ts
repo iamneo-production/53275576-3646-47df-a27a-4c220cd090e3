ac71990a10ffacbdf6fa2e16659ed1801821a7a3
senthilkumar <senthillkumar359@gmail.com>
2022-03-10 10:08:46 +0530

    enrollcourse_updated

--- angularapp1/src/app/enrolledcourse/enrolledcourse.component.ts
+++ angularapp1/src/app/enrolledcourse/enrolledcourse.component.ts
@@ -2,17 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Enrolledcoursepayload } from './enrolledcourse.payload';
 import { Router } from '@angular/router';
 import { FormGroup,FormControl,FormBuilder,Validators } from '@angular/forms';
-@Component({
+@Component
+(
+  {
   selector: 'app-enrolledcourse',
   templateUrl: './enrolledcourse.component.html',
   styleUrls: ['./enrolledcourse.component.css']
-})
-export class EnrolledcourseComponent implements OnInit {
+  }
+)
+
+export class EnrolledcourseComponent implements OnInit 
+
+{
 
   enrolledform:FormGroup;
   enrolledcoursepayload:Enrolledcoursepayload;
 
   constructor(private formbuilder:FormBuilder,private router:Router) 
+  
+  
   {
     this.enrolledcoursepayload={
       firstName:'',
@@ -35,26 +43,55 @@ export class EnrolledcourseComponent implements OnInit {
 
    }
 
-  ngOnInit(): void {
-    this.enrolledform=new FormGroup({ 
-      firstName:new FormControl('', [Validators.required]),
-      lastName:new FormControl('', [Validators.required]),
-      fatherName:new FormControl('', [Validators.required]),
-      motherName:new FormControl('', [Validators.required]),
+  ngOnInit(): void
+   {
+    this.enrolledform=new FormGroup
+    (
+
+      { 
+      firstName:new FormControl('', [Validators.required,Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')]),
+
+      lastName:new FormControl('',  [Validators.required,Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')]),
+
+      fatherName:new FormControl('',  [Validators.required,Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')]),
+
+      motherName:new FormControl('',  [Validators.required,Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')]),
+
       phoneNumber1:new FormControl('', [Validators.required,Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]),
-      emailId : new FormControl('', [Validators.required, Validators.email,Validators.pattern('^([a-zA-Z]+.*?)+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
-      age:new FormControl('', [Validators.required]),
+
+      emailId : new FormControl('', [Validators.required, Validators.email,Validators.pattern('^([a-zA-Z]+.*?)+@[a-z0-9.-]+\.[a-z]{2,4}$'),Validators.minLength(2)]),
+      
+      age:new FormControl('', [Validators.required,Validators.pattern("^(?:1[4-9]|[2-5][0-9]|60)$"),Validators.minLength(2)]),
+
       phoneNumber2:new FormControl('', [Validators.required,Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]),
-      SSLCorHSCMarks:new FormControl('', [Validators.required]),
+
+      SSLCorHSCMarks:new FormControl('', [Validators.required,Validators.pattern("^[0-9]*$")]),
+
       houseNo:new FormControl('', [Validators.required]),
-      streetName:new FormControl('', [Validators.required]),
-      areaName:new FormControl('', [Validators.required]),
-      pincode:new FormControl('', [Validators.required]),
-      state:new FormControl('', [Validators.required]),
-      nationality:new FormControl('', [Validators.required])
-    })
+
+      streetName:new FormControl('', [Validators.required,Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')]),
+
+      areaName:new FormControl('', [Validators.required,Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')]),
+
+      pincode:new FormControl('', [Validators.required,Validators.pattern('^[0-9]{6}$')]),
+
+      state:new FormControl('',  [Validators.required,Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')]),
+
+      nationality:new FormControl('',  [Validators.required,Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z ]*')])
+    }
+    
+    )
   }
-  onSubmit(e){
+  onSubmit(e)
+  {
     this.enrolledcoursepayload.firstName = this.enrolledform.get('firstname').value;
     this.enrolledcoursepayload.lastName = this.enrolledform.get('lastname').value;
     this.enrolledcoursepayload.fatherName = this.enrolledform.get('fathername').value;
@@ -62,6 +99,7 @@ export class EnrolledcourseComponent implements OnInit {
     this.enrolledcoursepayload.phoneNumber1 = this.enrolledform.get('phonenumber1').value;
     this.enrolledcoursepayload.phoneNumber2 = this.enrolledform.get('phonenumber2').value;
     this.enrolledcoursepayload.SSLCorHSCMarks = this.enrolledform.get('SSLCorHSCMarks').value;
+
     this.enrolledcoursepayload.age = this.enrolledform.get('age').value;
     this.enrolledcoursepayload.maleorfemale = this.enrolledform.get('maleorfemale').value;
     this.enrolledcoursepayload.emailId = this.enrolledform.get('email').value;
@@ -72,17 +110,25 @@ export class EnrolledcourseComponent implements OnInit {
     this.enrolledcoursepayload.state = this.enrolledform.get('state').value;
     this.enrolledcoursepayload.nationality= this.enrolledform.get('nationality').value;    
   }
-  viewAcademy(){
+
+  viewAcademy()
+  {
     console.log("Button Click");
 
   }
-  viewCourse(){
+  
+  viewCourse()
+  {
     console.log("Button Click");
   }
-  enrollnow(){
+
+  enrollnow()
+  {
     console.log("buttonclick");
   }
-  logout(){
+
+  logout()
+  {
     console.log("buttonclick");
     this.router.navigate(["/login"]);
   }
