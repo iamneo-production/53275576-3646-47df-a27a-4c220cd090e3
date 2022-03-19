cf485bf4e9e07652eb461981cd013d1cad49f898
Bala513010 <81249169+Bala513010@users.noreply.github.com>
2022-03-19 12:30:05 +0530

    Update review.component.ts

--- angularapp1/src/app/review/review.component.ts
+++ angularapp1/src/app/review/review.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder,FormControl,FormGroup,Validators } from '@angular/forms';
 import { ReviewPayload } from './review.payload';
+import { Review } from '../service/review';
+import { ReviewService } from '../service/review.service';
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
@@ -11,8 +13,9 @@ export class ReviewComponent implements OnInit
  {
    reviewform:FormGroup;
    reviewpayload:ReviewPayload;
+   review:Review =new Review();
 
-  constructor(private router:Router, private formbuilder:FormBuilder ) {
+  constructor(private router:Router, private formbuilder:FormBuilder,private reviewService:ReviewService ) {
     this.reviewpayload={
       name:'',
       comments:''
@@ -27,10 +30,25 @@ export class ReviewComponent implements OnInit
   ngOnInit(): void 
   {
     this.reviewform=new FormGroup({
-      name:new FormControl('',[Validators.required,Validators.minLength(4)]),
+      name:new FormControl('',[Validators.required,Validators.minLength(3)]),
       comments:new FormControl('',[Validators.required])
     })
   
+  }
+  saveReview(){
+  
+    console.log(this.review);
+  this.reviewService.createReview(this.review).subscribe(data=>{
+    console.log(this.review);
+  },
+    error => console.log(this.review));
+}
+  addreview(){
+    console.log(this.review);
+    
+    this.saveReview();
+    console.log("success");
+    alert("Student Added Successfully!!!");
   }
   onSubmit(e){
     this.reviewpayload.name=this.reviewform.get('name').value,
