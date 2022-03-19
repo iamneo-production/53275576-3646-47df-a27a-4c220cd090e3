7a75f8c6a81c72327f7857ec148b67ec0e1e56fe
Bala513010 <81249169+Bala513010@users.noreply.github.com>
2022-03-19 11:59:42 +0530

    Create ReviewModel.java

---
+++ springapp/src/main/java/com/examly/springapp/model/ReviewModel.java
@@ -0,0 +1,54 @@
+package com.examly.springapp.model;
+import javax.persistence.*;
+import java.util.Objects;
+@Entity
+@Table(name="review")
+public class ReviewModel {
+
+    @Id
+    @Column(name="reviewId")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer reviewId;
+
+    ReviewModel(){
+    }
+
+    public ReviewModel( String reviewName, String reviewComments, Integer reviewStars) {
+        this.reviewName = reviewName;
+        this.reviewComments = reviewComments;
+        this.reviewStars = reviewStars;
+        
+    }
+
+    @Column(name="reviewName")
+    private String reviewName;
+
+    @Column(name="reviewComments")
+    private String reviewComments;
+
+    @Column(name="reviewStars")
+    private Integer reviewStars;
+
+    public String getReviewName() {
+        return reviewName;
+    }
+
+    public void setReviewName(String reviewName) {
+        this.reviewName = reviewName;
+    }
+     public String getReviewComments() {
+        return reviewComments;
+    }
+
+    public void setReviewComments(String reviewComments) {
+        this.reviewComments = reviewComments;
+    }
+
+    public int getReviewStars(){
+        return reviewStars;
+    }
+    public void setReviewStars(int reviewStars){
+        this.reviewStars = reviewStars;
+    }
+
+}
