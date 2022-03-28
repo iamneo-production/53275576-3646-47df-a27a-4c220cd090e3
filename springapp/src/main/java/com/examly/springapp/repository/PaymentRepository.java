8781fd6e4083637b89bf8d152a5501ed1dbcb274
vtu11128 <83691018+vtu11128@users.noreply.github.com>
2022-03-28 17:11:45 +0530

    Update PaymentRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/PaymentRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/PaymentRepository.java
@@ -1,12 +1,12 @@
-//package repository;
-package com.examly.springapp.repository;
+//package repository ;
+package com.examly.springapp.repository ;
+ 
+import com.examly.springapp.model.PaymentModel ;
 
-import com.examly.springapp.model.PaymentModel;
+import org.springframework.data.jpa.repository.JpaRepository ;
+import org.springframework.stereotype.Repository ;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<PaymentModel, String> {
-
-}
+@Repository 
+public interface PaymentRepository extends JpaRepository<PaymentModel, String>  {
+ 
+ }
